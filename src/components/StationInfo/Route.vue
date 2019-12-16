2d451aa02a7f03cf5d4d10099e989f20c6583980
loney <geo.giatrakos@gmail.com>
2019-12-16 13:13:29 +0200

    Add station title

--- src/components/StationInfo/Route.vue
+++ src/components/StationInfo/Route.vue
@@ -1,13 +1,31 @@
 <template lang="html">
   <div >
-    <p>{{ id }}</p>
+    <v-container>
+        <v-layout row wrap>
+          <v-flex xs1 sm1 md1 >
+            <v-card flat color="primary">
+            <v-card-title class="justify-center white--white" >{{routes[0].number}}</v-card-title>
+            </v-card>
+          </v-flex>
+          <v-flex xs11 sm11 md11 >
+            <v-card  color="white">
+            <v-card-title class="justify-center white--white" >{{routes[0].direction}}</v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
   </div>
 </template>
 
 <script>
 export default {
   name : 'Route' ,
-  props : ['id']
+  props : ['id'] ,
+  data() {
+    return {
+      routes : [{id:2,number:136,direction:'Προς Στ. Φιξ'}]
+    }
+  }
 }
 </script>
 
