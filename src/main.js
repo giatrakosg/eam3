4fcb8e377a8c7cafb26a94bd6fedcf265d551001
loney <geo.giatrakos@gmail.com>
2019-12-21 20:33:47 +0200

    fix merge issue

--- src/main.js
+++ src/main.js
@@ -8,18 +8,10 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import BootstrapVue from 'bootstrap-vue'
 import '@fortawesome/fontawesome-free/css/all.css'
-import Breadcrumb from './components/Breadcrumb/Breadcrumb'
 
 import { LMap, LTileLayer, LMarker , LPolyline } from 'vue2-leaflet';
-import { Icon } from 'leaflet'
 
 import 'leaflet/dist/leaflet.css'
-
-
-/////////SECTION OF ERROR IN MAP////////////////////////////
-
-/////////////////////////////////////////////////////////
-
 
 Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
