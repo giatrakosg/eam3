80f3488cf0616e1244609974d5fb7c4f9b0a9313
loney <geo.giatrakos@gmail.com>
2019-12-22 23:50:19 +0200

    create traffic component

---
+++ src/components/StationInfo/Traffic.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="">
+    <v-card>
+
+      <v-card-title>Πληροφορίες Κίνησης στις  {{this.timestamp}}</v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+export default {
+  name : 'Traffic' ,
+  data () {
+    return {
+      timestamp : ''
+    }
+  },
+  created() {
+      setInterval(this.getNow, 1000);
+  },
+  methods: {
+      getNow: function() {
+          this.timestamp = moment().format('h:mm');
+      }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,31 @@
 <template>
-  <v-row justify="center" dense>
-
-    <v-col >
-
-      <v-img  contain
-                   src="../assets/athena-card.png">
-      <SearchRouteForm class="m-5"/>
-      </v-img>
-
-
-    </v-col>
-
-  </v-row>
+  <div >
+    <v-row justify="center" dense>
+      <v-col >
+        <v-img  contain
+                     src="../assets/athena-card.png">
+        <SearchRouteForm class="m-5"/>
+        </v-img>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <Traffic />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 
 
 import SearchRouteForm from "../components/Home/HomeSearchRouteForm";
+import Traffic from "../components/StationInfo/Traffic"
 export default {
   name: 'Home',
-  components: {SearchRouteForm},
+  components: {
+    SearchRouteForm ,
+    Traffic
+  },
 };
 </script>
