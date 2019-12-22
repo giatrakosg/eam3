15940140021c4cdbe8d1f3a0e6c1ec2302fa6d85
loney <geo.giatrakos@gmail.com>
2019-12-22 13:35:23 +0200

    Add vee-validate package for form validation

--- src/components/Auth/LoginForm.vue
+++ src/components/Auth/LoginForm.vue
@@ -1,15 +1,58 @@
-<template lang="html">
+<template>
   <div class="">
-    <v-form>
-    </v-form>
+    <v-layout row>
+      <v-flex sm2 md2 lg2>
+        <v-spacer></v-spacer>
+      </v-flex>
+      <v-flex sm8 md8 lg8>
+        <validation-provider rules="required" v-slot="{ errors }">
+          <v-text-field solo v-model="name">
+          </v-text-field>
+          <span>{{ errors[0] }}</span>
+        </validation-provider>
+      </v-flex>
+      <v-flex sm2 md2 lg2>
+        <v-spacer></v-spacer>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex sm2 md2 lg2>
+        <v-spacer></v-spacer>
+      </v-flex>
+      <v-flex sm8 md8 lg8>
+        <validation-provider rules="required" v-slot="{ errors }">
+          <v-text-field solo v-model="password">
+          </v-text-field>
+          <span>{{ errors[0] }}</span>
+        </validation-provider>
+      </v-flex>
+      <v-flex sm2 md2 lg2>
+        <v-spacer></v-spacer>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
+import { extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+import { ValidationProvider } from 'vee-validate';
+
+extend('required', {
+  ...required,
+  message: 'This field is required'
+});
 export default {
-  name : 'LoginForm'
+  name : 'LoginForm' ,
+  components : {
+    ValidationProvider
+  },
+  data () {
+    return {
+      value : ''
+    }
+  }
 }
 </script>
-
 <style lang="css" scoped>
 </style>
