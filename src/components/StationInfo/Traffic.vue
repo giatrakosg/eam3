08ceb3e7302cf91c0811e4f0f2bf1f7fd40d20fb
loney <geo.giatrakos@gmail.com>
2020-01-13 19:45:58 +0200

    add list with warnings

--- src/components/StationInfo/Traffic.vue
+++ src/components/StationInfo/Traffic.vue
@@ -1,106 +1,46 @@
 <template lang="html">
   <div class="">
-    <v-card>
-      <v-card-title style="background:#1565C0 ">
-        <span style="color: aliceblue">
-          <v-icon> mdi-refresh </v-icon>
-          Πληροφορίες Κίνησης στις  {{this.timestamp}}
-        </span>
-      </v-card-title>
-      <v-layout row wrap>
-        <v-flex sm2 md2 lg2>
-          <v-card-text>
-              <v-img
-                height="40"
-                width="40"
-                src="../../assets/green_logo.jpg">
-              </v-img>
-          </v-card-text>
-        </v-flex>
-        <v-flex sm10 md10 lg10>
-          <v-card-text>
-            <v-chip
-            color="success"
-            >
-              1
-            </v-chip>
-            <v-chip
-            color="orange"
-            >
-              2
-            </v-chip>
-            <v-chip
-            color="success"
-            >
-              3
-            </v-chip>
-          </v-card-text>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex sm2 md2 lg2>
-          <v-card-text>
-              <v-img
-              height="40"
-              width="40"
-                src="../../assets/indigo_logo.jpg">
-              </v-img>
-          </v-card-text>
-        </v-flex>
-        <v-flex sm10 md10 lg10>
-          <v-card-text>
-            <v-chip
-            color="success"
-            >
-              1
-            </v-chip>
-            <v-chip
-            color="red"
-            >
-              2
-            </v-chip>
-            <v-chip
-            color="success"
-            >
-              3
-            </v-chip>
-          </v-card-text>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex sm2 md2 lg2>
-          <v-card-text>
-              <v-img
-              height="40"
-              width="40"
-                src="../../assets/orange_logo.jpg">
-              </v-img>
-          </v-card-text>
-        </v-flex>
-        <v-flex sm10 md10 lg10>
-          <v-card-text>
-            <v-chip v-model="value" filter color="success">Καμια διακοπη</v-chip>
-          </v-card-text>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex sm2 md2 lg2>
-          <v-card-text>
-              <v-img
-              height="40"
-              width="40"
-                src="../../assets/blue_logo.jpg">
-              </v-img>
-          </v-card-text>
-        </v-flex>
-        <v-flex sm10 md10 lg10>
-          <v-card-text>
-            <v-chip v-model="value" filter color="success">Καμια διακοπη</v-chip>
-          </v-card-text>
-        </v-flex>
-      </v-layout>
-
+  <v-row align="center">
+    <v-card
+      class="mx-auto"
+      max-width="400"
+      tile
+    >
+      <v-list
+        :disabled="disabled"
+        :dense="dense"
+        :two-line="twoLine"
+        :three-line="threeLine"
+        :shaped="shaped"
+        :flat="flat"
+        :subheader="subheader"
+        :sub-group="subGroup"
+        :nav="nav"
+        :avatar="avatar"
+        :rounded="rounded"
+      >
+        <v-subheader
+        >
+            ΚΑΘΥΣΤΕΡΗΣΕΙΣ
+        </v-subheader>
+        <v-list-item-group v-model="item" color="primary">
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :inactive="inactive"
+          >
+            <v-list-item-avatar v-if="avatar">
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-html="item.title"></v-list-item-title>
+              <v-list-item-subtitle v-if="twoLine || threeLine" v-html="item.subtitle"></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-card>
+  </v-row>
   </div>
 </template>
 
@@ -111,7 +51,42 @@ export default {
   data () {
     return {
       timestamp : '' ,
-      value : true
+      value : true ,
+      item: 5,
+      items: [
+        {
+          avatar: 'https://i.pinimg.com/originals/02/b7/04/02b704dcc8cb5084a806f2915acb23af.jpg',
+          title: 'Διακοπη Λειτουργιας Μετρο',
+          subtitle: "<span class='text--primary'>Γραμμη 2</span> &mdash; Λογω εργασιων η γραμμη 2 του μετρο θα παραμεινει κλειστη",
+        },
+        {
+          avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Bus-logo.svg/454px-Bus-logo.svg.png',
+          title: 'Καθυστερησεις γραμμη 136,137',
+          subtitle: "<span class='text--primary'>136-137</span> &mdash; Καθυστερησεις στις γραμμες 136-137 λογω συμφορησης",
+        },
+        {
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeH8EeTz-89gZSd86VekUrwuRJDkVhzmzz6LrDi7lC1gj1D1zF3w&s',
+          title: 'Διακοπη Λειτουργιας Τραμ',
+          subtitle: "<span class='text--primary'>Σταση Φιξ εως Συνταγμα</span> &mdash; Δεν πραγματοποιουνται δρομολογια απο τις στασεις Φιξ εως και Συνταγμα",
+        },
+        {
+          avatar: 'https://i.pinimg.com/originals/02/b7/04/02b704dcc8cb5084a806f2915acb23af.jpg',
+          title: 'Μη Λειτουργια ανελκυστηρα',
+          subtitle: "<span class='text--primary'>Σταση Ευαγγελισμος</span> &mdash; Δεν ειναι σε λειτουργια ο ανελκυστηρας στην σταση Ευαγγελισμος",
+        },
+      ],
+      disabled: false,
+      dense: false,
+      twoLine: false,
+      threeLine: true,
+      shaped: false,
+      flat: false,
+      subheader: false,
+      inactive: false,
+      subGroup: false,
+      nav: false,
+      avatar: true,
+      rounded: false,
     }
   },
   created() {
